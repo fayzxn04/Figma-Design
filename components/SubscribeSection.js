ebbe25822138d2b8644096adc5343dae54e06dd1
Fayzan Shaikh <iamfaizan98@gmail.com>
2025-07-16 10:05:06 +0530

    Fix Error

--- components/SubscribeSection.js
+++ components/SubscribeSection.js
@@ -1,11 +1,18 @@
 "use client";
+import { useState } from "react";
 import { Mail, Send } from "lucide-react";
 
 export default function Subscribe() {
+  const [email, setEmail] = useState("");
+
+  const handleSubscribe = () => {
+    console.log("Subscribed with:", email);
+    setEmail(""); // Clear the input field
+  };
   return (
     <section className="relative bg-[#e0e7ff] rounded-2xl px-6 sm:px-10 lg:px-32 py-16 mt-20 max-w-7xl mx-auto">
       {/* Paper plane */}
-      <div className="absolute -top-[10px] -right-[18px] bg-[#4f46e5] p-3 rounded-full shadow-lg z-10 ">
+      <div className=" hidden md:block absolute -top-[10px] -right-[18px] bg-[#4f46e5] p-3 rounded-full shadow-lg z-10 ">
         <Send className="text-white w-5 h-5 " />
       </div>
 
@@ -25,10 +32,15 @@ export default function Subscribe() {
             <input
               type="email"
               placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="flex-1 bg-transparent outline-none text-sm"
             />
           </div>
-          <button className="bg-gradient-to-r from-orange-400 to-orange-500 text-white px-6 py-3 rounded-md shadow hover:opacity-90 transition duration-200">
+          <button
+            className="bg-gradient-to-r from-orange-400 to-orange-500 text-white px-6 py-3 rounded-md shadow hover:opacity-90 transition duration-200"
+            onClick={handleSubscribe}
+          >
             Subscribe
           </button>
         </div>
